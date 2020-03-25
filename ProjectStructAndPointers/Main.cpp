39a629308ad2a7260ed115227085b65a2cc206d3
Mark G <mark.garcia.8001@gmail.com>
2020-03-25 15:16:05 -0700

    added the sort functions

--- ProjectStructAndPointers/Main.cpp
+++ ProjectStructAndPointers/Main.cpp
@@ -16,18 +16,35 @@ struct Student {
 	string homeTown;
 };
 
+int menuCoice();
+void populate(Student *[]);
+void display(Student *[]);
+void swap(Student *[], Student *[]);
+void sortName(Student *[]);
+void sortID(Student *[]);
+void sortGrade(Student *[]);
+void sortBirthday(Student *[]);
+void sortHTown(Student *[]);
+
 int main() {
 	Student *s1[10];
 	populate(s1);
-	cout << s1;
+	cout << s1[2]->name << endl;
+	cout << s1[2]->SID << endl;
+	cout << s1[2]->bDay.toString() << endl;
+	cout << s1[2]->grade << endl;
+	cout << s1[2]->homeTown << endl;
 	return 0;
 };
 
+//populate the array with students
 void populate(Student *s[]) {
-	char grades[] = { 'A','B','C','D','F' };
-	string cities[] = {"Downey", "Longbeach", "Compton", "Carson", "San Fransisco", "San Antonio", "Dalas", "Bronx", "Queens", "Brooklyn", "Harlem", "Las Vegas", "Detroit"};
+	string names[10] = {"Steve", "Jeremy", "Calvin", "Mark", "Michael", "Josie", "Linda", "Maddison", "Tyler", "Wendy"};
+	char grades[5] = { 'A','B','C','D','F' };
+	string cities[10] = {"Downey", "Long Beach", "Compton", "Carson", "Dalas", "Bronx", "Queens", "Brooklyn", "Harlem", "Detroit"};
 	for (int i = 0; i < 10; i++) {
 		s[i] = new Student;
+		strcpy_s(s[i]->name, names[i].c_str());
 		s[i]->SID = rand() % 9000 + 1000;
 		int randGrade = rand() % 5;
 		s[i]->grade = grades[randGrade];
@@ -35,7 +52,7 @@ void populate(Student *s[]) {
 		int randMonth = rand() % 13;
 		int randYear = rand() % 6 + 1999;
 		s[i]->bDay = myDate(randMonth, randDay, randYear);
-		int randCity = rand() % 14;
+		int randCity = rand() % 10;
 		s[i]->homeTown = cities[randCity];
 	}
 }
